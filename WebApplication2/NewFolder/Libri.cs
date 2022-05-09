013fa74e58933de995dfaee704a2869c1b872d7a
bardha cocaj <fc52610@ubt-uni.net>
2022-05-09 15:36:45 +0200

    Add project files.

---
+++ WebApplication2/NewFolder/Libri.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.NewFolder
+{
+    public class Libri
+    {
+        public int Libri_ID { get; set; }
+
+        public string Titulli { get; set; }
+        public string Faqet { get; set; }
+        public string Disponueshmeria { get; set; }
+        public string Viti { get; set; }
+        public string PershkrimiLibrit { get; set; }
+
+        public string EmriKategorise { get; set; }
+        public string photolibri { get; set; }
+
+    }
+}
